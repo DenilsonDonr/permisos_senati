b936738272d631c8759071210e114741786f1b6b
Denilson Jhon <denilsontrillo@gmail.com>
2024-10-17 09:43:22 -0500

    Plantilla LT

---
+++ app/config/App.php
@@ -0,0 +1,11 @@
+<?php
+
+// Contantes globales
+define('SERVERURL', 'http://localhost/permisos_senati');
+define('COMPANY', 'SENATI');
+define('VERSION', '1.0');
+
+// Configuración horario
+date_default_timezone_set("America/Lima");
+
+// Recursos helpers
---
+++ views/layout/foot.php
@@ -0,0 +1,7 @@
+    <script src="<?php echo SERVERURL ?>/plugins/jquery/jquery.min.js"></script>
+    <!-- Bootstrap 4 -->
+    <script src="<?php echo SERVERURL ?>/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="<?php echo SERVERURL ?>/dist/js/adminlte.min.js"></script>
+</body>
+</html>
